11150f3f4ceb8719956bf34bce5cc68775b71ecc
CHIGODI <chiegody254@gmail.com>
2024-06-15 19:26:41 +0300

    Add content to landing page Add styling and responsiveness to landing page

--- chigow_stores/chigow_stores/settings.py
+++ chigow_stores/chigow_stores/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'chigow_stores.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +117,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'e_store/static',
+]
+
+
+# Ensure STATIC_ROOT is defined for when you collectstatic for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (Uploaded by users)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- chigow_stores/e_store/views.py
+++ chigow_stores/e_store/views.py
@@ -1,9 +1,11 @@
 """A Django view that renders the home page."""
 from django.shortcuts import render
+import uuid
 
 
 
 def home(request):
     """A view that displays the home page."""
-    return render(request, 'home.html')
+    cache_id = str(uuid.uuid4())
+    return render(request, 'home.html', {'cache_id': cache_id})
 
